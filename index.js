b20fd856172a015265c36e4c36358ee468d87322
Gilbert <steven.n.gilbert@gmail.com>
2017-04-01 21:51:30 -0400

    fix: Added code comments

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-function initialize() {
-  window.addEventListener('keydown', (e) => {
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    const message = document.getElementById('message');
-    const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+function dropThatBeat(e) {
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  const message = document.getElementById('message');
+  const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
-    if (!key) {
-      message.classList.add('fade');
-      message.addEventListener('transitionend', (ev) => {
-        ev.target.classList.remove('fade');
-      });
-    } else {
-      key.setAttribute('class', 'key playing');
-      sound.currentTime = 0;
-      sound.play();
+  if (!key) {
+    // If a wrong press is pressed, alert user to play correct key
+    message.classList.add('fade');
+    message.addEventListener('transitionend', (ev) => {
+      ev.target.classList.remove('fade');
+    });
+  } else {
+    // Transforms key styles and plays beat
+    key.setAttribute('class', 'key playing');
+    sound.currentTime = 0;
+    sound.play();
 
-      key.addEventListener('transitionend', (ev) => {
-        ev.target.classList.remove('playing');
-      }, false);
-    }
-  });
+    // Returns key to original css value after transition ends
+    key.addEventListener('transitionend', (ev) => {
+      ev.target.classList.remove('playing');
+    }, false);
+  }
 }
 
-window.onload = initialize;
+window.onload = () => {
+  window.addEventListener('keydown', dropThatBeat);
+};
